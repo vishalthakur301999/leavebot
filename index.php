5359d56fcd7b1be8645752b316504a93505b2448
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-22 15:40:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,16 +16,38 @@ if($method == 'POST'){
 
     $flag = "";
     $flag = $json->queryResult->outputContexts[0]->parameters->flag;
-
-    if(strcmp("check",$flag)==0){
+    if(strcmp("login",$flag)){
         $uname = $json->queryResult->outputContexts[1]->parameters->person->name;
-        /*$chkquery = "select * from Leave_Balance where username = $uname";
-        mysqli_query($conn,$query);*/
-        $speech1 = "You have these many Leaves left";
-        $response = new \stdClass();
-        $response->fulfillmentText = $speech1;
-        $response->source = "webhook";
-        echo json_encode($response);
+        $chkquery = "select * from Leave_Balance where username = $uname";
+        mysqli_query($conn,$query);
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            echo "Hey ".$uname."! What do you want to do today?";
+        }
+        else{
+            $speech1 = "Invalid user";
+            $response = new \stdClass();
+            $response->fulfillmentText = $speech1;
+            $response->source = "webhook";
+            echo json_encode($response);
+        }
+
+    }
+    else if(strcmp("check",$flag)==0){
+        $uname = $json->queryResult->outputContexts[1]->parameters->person->name;
+        $chkquery = "select * from Leave_Balance where username = $uname";
+        mysqli_query($conn,$query);
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $speech1 = "You have $row[CL_Balance] Casual Leaves and $row[PL_Balance] Paid Leaves Left";
+            $response = new \stdClass();
+            $response->fulfillmentText = $speech1;
+            $response->source = "webhook";
+            echo json_encode($response);
+        }
+
+
     }
     else if(strcmp("apply",$flag)==0){
         $uname = $json->queryResult->outputContexts[1]->parameters->person->name;
