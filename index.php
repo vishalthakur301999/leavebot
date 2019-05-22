a43d2a0c5954324a9218e54a9d6fdd84ddcc41ab
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-22 17:15:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -59,15 +59,82 @@ if($method == 'POST'){
 
     }
     else if(strcmp("apply",$flag)==0){
-        $uname = $json->queryResult->outputContexts[1]->parameters->person->name;
-        /*$chkquery = "select * from Leave_Balance where username = $uname";
-        $query = "INSERT INTO Bookings(ID, NoM, DateTime) VALUES ('',$number,'$date_time')";
-        mysqli_query($conn,$query);*/
-        $speech2 = "You want to apply leave";
-        $response = new \stdClass();
-        $response->fulfillmentText = $speech2;
-        $response->source = "webhook";
-        echo json_encode($response);
+        $uname = $json->queryResult->outputContexts[0]->parameters->person->name;
+        $from = $json->queryResult->outputContexts[0]->parameters->fromdate;
+        $from = substr($from,0,10);
+        $to = $json->queryResult->outputContexts[0]->parameters->todate;
+        $to = substr($to,0,10);
+        $dateDiff = dateDiffInDays($from, $to);
+        $type = $json->queryResult->outputContexts[0]->parameters->type;
+        $chkquery = "select * from Leave_Balance where username = '$uname'";
+        $result = mysqli_query($conn, $chkquery);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            if(strcmp("PL",$type)==0){
+                $balance = "$row[PL_Balance]";
+                $balance = (int)$balance;
+                if($dateDiff<=$balance){
+                    $query = "INSERT INTO Applied_Leaves(Lid,Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ('',$row[Eid],'$from','$to','$type','')";
+
+                    $n = $balance-$dateDiff;
+                    $query2 = "UPDATE Leave_Balance SET PL_Balance=$n WHERE Eid = '$row[Eid]'";
+                    $res = mysqli_query($conn, $query);
+                    if (!$res) {
+                        die('Invalid query: ' . mysqli_error($conn));
+                    }
+                    $res2 = mysqli_query($conn, $query2);
+                    if (!$res) {
+                        die('Invalid query: ' . mysqli_error($conn));
+                    }
+
+                    $speech1 = "Leave Applied Successfully!";
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $speech1;
+                    $response->source = "webhook";
+                    echo json_encode($response);
+                }
+                else{
+                    $speech1 = "Leave Application Unsuccessful, Insufficient Balance!";
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $speech1;
+                    $response->source = "webhook";
+                    echo json_encode($response);
+                }
+            }
+
+            else if(strcmp("CL",$type)==0){
+                $balance = "$row[CL_Balance]";
+                $balance = (int)$balance;
+                if($dateDiff<=$balance){
+                    $query = "INSERT INTO Applied_Leaves(Lid,Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ('',$row[Eid],'$from','$to','$type','')";
+
+                    $n = $balance-$dateDiff;
+                    echo $dateDiff;
+                    $query2 = "UPDATE Leave_Balance SET CL_Balance=$n WHERE Eid = '$row[Eid]'";
+                    $res = mysqli_query($conn, $query);
+                    if (!$res) {
+                        die('Invalid query: ' . mysqli_error($conn));
+                    }
+                    $res2 = mysqli_query($conn, $query2);
+                    if (!$res) {
+                        die('Invalid query: ' . mysqli_error($conn));
+                    }
+                    $speech1 = "Leave Applied Successfully!";
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $speech1;
+                    $response->source = "webhook";
+                    echo json_encode($response);
+                }
+                else{
+                    $speech1 = "Leave Application Unsuccessful, Insufficient Balance!";
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $speech1;
+                    $response->source = "webhook";
+                    echo json_encode($response);
+                }
+            }
+
+        }
     }
 }
 else
