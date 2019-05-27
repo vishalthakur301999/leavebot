0e770f4af4109d308ca98880d6f4889c97bcffbc
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-27 16:15:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,19 +9,15 @@ function dateDiffInDays($date1, $date2)
 {
     // Calulating the difference in timestamps
     $diff = strtotime($date2) - strtotime($date1);
-
     // 1 day = 24 hours
     // 24 * 60 * 60 = 86400 seconds
     return abs(round($diff / 86400));
 }
 $method = $_SERVER['REQUEST_METHOD'];
-
 // Process only when method is POST
 if($method == 'POST'){
     $requestBody = file_get_contents('php://input');
-
     $json = json_decode($requestBody);
-
     $flag = "";
     $flag = $json->queryResult->outputContexts[0]->parameters->flag;
     if(strcmp("login",$flag)==0){
@@ -42,7 +38,6 @@ if($method == 'POST'){
             $response->source = "webhook";
             echo json_encode($response);
         }
-
     }
     else if(strcmp("check",$flag)==0){
         $uname = $json->queryResult->outputContexts[1]->parameters->person->name;
@@ -50,7 +45,7 @@ if($method == 'POST'){
         $result = mysqli_query($conn, $chkquery);
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
-            $speech1 = "CL: $row[CL_Balance], PL: $row[PL_Balance]";
+            $speech1 = "CL: $row[CL_Balance], PL: $row[PL_Balance], SL: $row[SL_Balance]";
             $response = new \stdClass();
             $response->fulfillmentText = $speech1;
             $response->source = "webhook";
@@ -63,14 +58,26 @@ if($method == 'POST'){
             $response->source = "webhook";
             echo json_encode($response);
         }
-
+    }
+    else if(strcmp("confirm",$flag)==0){
+        $uname = $json->queryResult->outputContexts[0]->parameters->person->name;
+        $from = $json->queryResult->outputContexts[0]->parameters->from;
+        $from = substr($from,0,10);
+        $to = $json->queryResult->outputContexts[0]->parameters->to;
+        $to = substr($to,0,10);
+        $dateDiff = dateDiffInDays($from, $to);
+        $speech1 = "Confirm Leave of $dateDiff days?";
+        $response = new \stdClass();
+        $response->fulfillmentText = $speech1;
+        $response->source = "webhook";
+        echo json_encode($response);
 
     }
     else if(strcmp("apply",$flag)==0){
         $uname = $json->queryResult->outputContexts[0]->parameters->person->name;
-        $from = $json->queryResult->outputContexts[0]->parameters->fromdate;
+        $from = $json->queryResult->outputContexts[0]->parameters->from;
         $from = substr($from,0,10);
-        $to = $json->queryResult->outputContexts[0]->parameters->todate;
+        $to = $json->queryResult->outputContexts[0]->parameters->to;
         $to = substr($to,0,10);
         $dateDiff = dateDiffInDays($from, $to);
         $type = $json->queryResult->outputContexts[0]->parameters->type;
@@ -83,7 +90,6 @@ if($method == 'POST'){
                 $balance = (int)$balance;
                 if($dateDiff<=$balance){
                     $query = "INSERT INTO Applied_Leaves(Lid,Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ('',$row[Eid],'$from','$to','$type','')";
-
                     $n = $balance-$dateDiff;
                     $query2 = "UPDATE Leave_Balance SET PL_Balance=$n WHERE Eid = '$row[Eid]'";
                     $res = mysqli_query($conn, $query);
@@ -94,7 +100,6 @@ if($method == 'POST'){
                     if (!$res) {
                         die('Invalid query: ' . mysqli_error($conn));
                     }
-
                     $speech1 = "Leave Applied Successfully!";
                     $response = new \stdClass();
                     $response->fulfillmentText = $speech1;
@@ -109,16 +114,44 @@ if($method == 'POST'){
                     echo json_encode($response);
                 }
             }
-
             else if(strcmp("CL",$type)==0){
                 $balance = "$row[CL_Balance]";
                 $balance = (int)$balance;
                 if($dateDiff<=$balance){
                     $query = "INSERT INTO Applied_Leaves(Lid,Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ('',$row[Eid],'$from','$to','$type','')";
-
                     $n = $balance-$dateDiff;
                     echo $dateDiff;
                     $query2 = "UPDATE Leave_Balance SET CL_Balance=$n WHERE Eid = '$row[Eid]'";
+                    $res = mysqli_query($conn, $query);
+                    if (!$res) {
+                        die('Invalid query: ' . mysqli_error($conn));
+                    }
+                    $res2 = mysqli_query($conn, $query2);
+                    if (!$res) {
+                        die('Invalid query: ' . mysqli_error($conn));
+                    }
+                    $speech1 = "Leave Applied Successfully!";
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $speech1;
+                    $response->source = "webhook";
+                    echo json_encode($response);
+                }
+                else{
+                    $speech1 = "Leave Application Unsuccessful, Insufficient Balance!";
+                    $response = new \stdClass();
+                    $response->fulfillmentText = $speech1;
+                    $response->source = "webhook";
+                    echo json_encode($response);
+                }
+            }
+            else if(strcmp("SL",$type)==0){
+                $balance = "$row[SL_Balance]";
+                $balance = (int)$balance;
+                if($dateDiff<=$balance){
+                    $query = "INSERT INTO Applied_Leaves(Lid,Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ('',$row[Eid],'$from','$to','$type','')";
+                    $n = $balance-$dateDiff;
+                    echo $dateDiff;
+                    $query2 = "UPDATE Leave_Balance SET SL_Balance=$n WHERE Eid = '$row[Eid]'";
                     $res = mysqli_query($conn, $query);
                     if (!$res) {
                         die('Invalid query: ' . mysqli_error($conn));
@@ -148,9 +181,4 @@ else
 {
     echo "Method not allowed";
 }
-
 ?>
-
-
-
-
