2ab07e87478285fb76f8568c7c5d03c12786d571
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-28 14:11:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ function dateDiffInDays($date1, $date2)
 {
     // Calulating the difference in timestamps
     $diff = strtotime($date2) - strtotime($date1);
+    if($diff==0){
+        $diff=1;
+    }
     // 1 day = 24 hours
     // 24 * 60 * 60 = 86400 seconds
     return abs(round($diff / 86400));
