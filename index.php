59495ecc54c1555e68ab8d5ef84ec5b03d9fde1c
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-31 11:00:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ function dateDiffInDays($date1, $date2)
 }
 $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST') {
-
 // Process only when method is POST
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
@@ -177,7 +176,7 @@ else if(strcmp("graph", $flag) == 0){
      $chkquery4 = "select * from absentemployee  where Time_period = 'this_month'";
      $result4 = mysqli_query($conn, $chkquery4);
      $row4=mysqli_fetch_assoc($result4);
-     $speech1 = "$row1['absentees'],$row2['absentees'],$row3['absentees'], $row4['absentees'";
+	 $speech1 = "$row1[absentees],$row2[absentees],$row3[absentees], $row4[absentees]";
      $response = new \stdClass();
      $response->fulfillmentText = $speech1; 
      $response->source = "webhook";
