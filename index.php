985add73a8436f16e646b6dd5336dce6cedaf6cd
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-31 10:07:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if($method == 'POST') {
     $flag = $json->queryResult->outputContexts[0]->parameters->flag;
     if (strcmp("login", $flag) == 0) {
         $uname = $json->queryResult->outputContexts[0]->parameters->person->name;
-        $query = "select * from Leave_Balance where username = '$uname'";
+        $query = "select * from leave_balance where username = '$uname'";
         $result = mysqli_query($conn, $query);
         if (mysqli_num_rows($result) > 0) {
             $speech1 = "Hey " . $uname . "! What do you want to do today?";
@@ -38,7 +38,7 @@ if($method == 'POST') {
         }
     } else if (strcmp("check", $flag) == 0) {
         $uname = $json->queryResult->outputContexts[1]->parameters->person->name;
-        $chkquery = "select * from Leave_Balance where username = '$uname'";
+        $chkquery = "select * from leave_balance where username = '$uname'";
         $result = mysqli_query($conn, $chkquery);
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
@@ -76,7 +76,7 @@ if($method == 'POST') {
         $dateDiff = dateDiffInDays($from, $to);
         $dateDiff = $dateDiff + 1;
         $type = $json->queryResult->outputContexts[0]->parameters->type;
-        $chkquery = "select * from Leave_Balance where username = '$uname'";
+        $chkquery = "select * from leave_balance where username = '$uname'";
         $result = mysqli_query($conn, $chkquery);
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
@@ -84,9 +84,9 @@ if($method == 'POST') {
                 $balance = "$row[PL_Balance]";
                 $balance = (int)$balance;
                 if ($dateDiff <= $balance) {
-                    $query = "INSERT INTO Applied_Leaves(Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ($row[Eid],'$from','$to','$type','')";
+                    $query = "INSERT INTO applied_leavess(Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ($row[Eid],'$from','$to','$type','')";
                     $n = $balance - $dateDiff;
-                    $query2 = "UPDATE Leave_Balance SET PL_Balance=$n WHERE Eid = '$row[Eid]'";
+                    $query2 = "UPDATE leave_balance SET PL_Balance=$n WHERE Eid = '$row[Eid]'";
                     $res = mysqli_query($conn, $query);
                     if (!$res) {
                         die('Invalid query: ' . mysqli_error($conn));
@@ -111,9 +111,9 @@ if($method == 'POST') {
                 $balance = "$row[CL_Balance]";
                 $balance = (int)$balance;
                 if ($dateDiff <= $balance) {
-                    $query = "INSERT INTO Applied_Leaves(Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ($row[Eid],'$from','$to','$type','')";
+                    $query = "INSERT INTO applied_leavess(Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ($row[Eid],'$from','$to','$type','')";
                     $n = $balance - $dateDiff;
-                    $query2 = "UPDATE Leave_Balance SET CL_Balance=$n WHERE Eid = '$row[Eid]'";
+                    $query2 = "UPDATE leave_balance SET CL_Balance=$n WHERE Eid = '$row[Eid]'";
                     $res = mysqli_query($conn, $query);
                     if (!$res) {
                         die('Invalid query: ' . mysqli_error($conn));
@@ -138,9 +138,9 @@ if($method == 'POST') {
                 $balance = "$row[SL_Balance]";
                 $balance = (int)$balance;
                 if ($dateDiff <= $balance) {
-                    $query = "INSERT INTO Applied_Leaves(Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ($row[Eid],'$from','$to','$type','')";
+                    $query = "INSERT INTO applied_leavess(Eid,From_Date,To_Date,Leave_Type,Reason) VALUES ($row[Eid],'$from','$to','$type','')";
                     $n = $balance - $dateDiff;
-                    $query2 = "UPDATE Leave_Balance SET SL_Balance=$n WHERE Eid = '$row[Eid]'";
+                    $query2 = "UPDATE leave_balance SET SL_Balance=$n WHERE Eid = '$row[Eid]'";
                     $res = mysqli_query($conn, $query);
                     if (!$res) {
                         die('Invalid query: ' . mysqli_error($conn));
