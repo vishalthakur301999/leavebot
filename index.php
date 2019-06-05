fe819a3b8b71becb2a8c83c8f992859773656e7e
Vishal Thakur <vishalthakur301999@gmail.com>
2019-06-05 21:39:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -124,9 +124,9 @@ if($method == 'POST') {
         if (mysqli_num_rows($result) > 0) {
             $speech1 = "";
             while ($row = mysqli_fetch_assoc($result)) {
-                $speech1 = $speech1."$row[LeaveType]".":"."$row[Balance]"." , ";
+                $speech1 = $speech1." $row[LeaveType]".":"." $row[Balance]".", ";
             }
-            $speech1 = substr($speech1,0,-3);
+            $speech1 = substr($speech1,0,-2);
             $response = new \stdClass();
             $response->fulfillmentText = $speech1;
             $response->source = "webhook";
